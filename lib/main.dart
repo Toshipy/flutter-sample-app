7d40af70041e673058a782863e2cf4db7f05c801
Toshipy <arai1209devops@gmail.com>
2025-04-13 08:22:37 +0900

    rock-paper-scissors

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'デモ',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'デモ'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  // void showRobotResult() {
+  //
+  //   List<String> hands = ['pa', 'choki', 'gu'];
+  //   hands.shuffle();
+  //   String hand = hands.first;
+  //
+  //   Image image = Image.asset('assets/janken_$hand.png');
+  //
+  //   showDialog(context: context, builder: (context) {
+  //     return AlertDialog(
+  //       title: Text('ロボット'),
+  //       content: image
+  //     );
+  //   },);
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // showRobotResult();
+          },
+          child: Text('じゃんけん')
+            ),
+        ),
+      );
+  }
+}
+
